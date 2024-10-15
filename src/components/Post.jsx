21bb8b1e112528f983667741d95b9e27d58b055e
Bruna Dantas <b.dantassoares@gmail.com>
2024-10-15 06:23:48 -0300

    Componente Avatar finalizado.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { Avatar } from './Avatar';
 
 export function Post() {
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                <img className={styles.avatar} src="https://github.com/BrunaDan7as.png" />
+                <Avatar hasBorder src="https://github.com/BrunaDan7as.png" />
                 <div className={styles.authorInfo}>
                     <strong>Bruna Dantas</strong>
                     <span>FullStack Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine} from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -8,7 +9,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1727121798717-5dd950142c00?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/BrunaDan7as.png" />
+                <Avatar src="https://github.com/BrunaDan7as.png" />
 
                 <strong>Bruna Dantas</strong>
                 <span>FullStack Developer</span>
